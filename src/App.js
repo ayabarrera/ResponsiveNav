68d86b9d89fe3b1ce6f735b9cea37f66270df53b
ayabarrera <alyannabarrera@gmail.com>
2025-01-27 16:19:32 -0700

    added hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
+import './index.css';
 import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 import Home from './components/Home';
 import Work from './components/Works';
@@ -8,6 +9,12 @@ import Contact from './components/Contact';
 import ayalogo from './images/ayalogo.png';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(prevState => !prevState);
+  };
+
   return (
     <Router>
       <div>
@@ -16,12 +23,18 @@ function App() {
             <img src={ayalogo} alt="A Logo" />
           </div>
 
-          <ul className="nav-links">
-            <li><NavLink to="/" end>HOME</NavLink></li>
-            <li><NavLink to="/About">ABOUT</NavLink></li>
-            <li><NavLink to="/Work">WORKS</NavLink></li>
-            <li><NavLink to="/Contact">CONTACT</NavLink></li>
+          <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+            <li><NavLink to="/" end onClick={() => setIsOpen(false)}>HOME</NavLink></li>
+            <li><NavLink to="/About" onClick={() => setIsOpen(false)}>ABOUT</NavLink></li>
+            <li><NavLink to="/Work" onClick={() => setIsOpen(false)}>WORKS</NavLink></li>
+            <li><NavLink to="/Contact" onClick={() => setIsOpen(false)}>CONTACT</NavLink></li>
           </ul>
+
+          <div className="hamburger" onClick={toggleMenu}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
         </nav>
 
         <Routes>
@@ -31,7 +44,6 @@ function App() {
           <Route path="/Contact" element={<Contact />} />
         </Routes>
 
-      
         <footer className="footer">
           <p>&copy; 2025 Aya Barrera | All rights reserved.</p>
         </footer>
